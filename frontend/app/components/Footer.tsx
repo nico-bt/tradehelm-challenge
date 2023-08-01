83e553145d71eeffac4f172e3c581f1eb81138e7
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-08-01 14:09:29 -0300

    Add footer

---
+++ frontend/app/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        Challenge hecho con ❤ por{" "}
+        <a
+          href="https://www.linkedin.com/in/nico-battaglia/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Nico Battaglia
+        </a>
+      </div>
+    </footer>
+  )
+}
